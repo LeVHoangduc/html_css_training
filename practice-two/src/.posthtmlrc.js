bb12632d57f6aefca0c7a23acbb126d15ec29bde
LeVHoangduc <levhoangduc@gmail.com>
2023-07-13 16:46:25 +0700

    fix:modify the configuration file

--- practice-two/src/.posthtmlrc.js
+++
@@ -1,9 +0,0 @@
-const posthtmlInclude = require("posthtml-include");
-const config = {
-  plugins: [
-    posthtmlInclude({
-      root: __dirname + "/",
-    }),
-  ],
-};
-module.exports = config;
